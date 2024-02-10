1b0a0fd454c7543bec9d081973c5e4b74ea18476
“narendralovanshi” <narendralovanshi51@gmail.com>
2024-02-10 13:34:07 +0530

    test

---
+++ script.js
@@ -0,0 +1,20 @@
+// Get Full Name //
+
+let name = "Narendra";
+let lastname = "Lovanshi";
+
+console.log(name + " " + lastname);
+
+
+// Reverse string //
+
+let string ="narendra"
+let strLen = string.length;
+let  revStr = ""
+for ( let i = strLen-1;  i>=0; i--)
+{
+   revStr += string[i];
+}
+console.log(`${string} reverse is ${revStr}`);
+
+// find 
